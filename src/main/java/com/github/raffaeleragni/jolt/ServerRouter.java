95451bf0e6f5cbc9bbc53d65c529e48a96c23016
Raffaele Ragni <raffaele.ragni@gmail.com>
2020-06-27 23:19:52 +0200

    hold the bytes so they can be parsed twice

--- src/main/java/com/github/raffaeleragni/jolt/ServerRouter.java
+++ src/main/java/com/github/raffaeleragni/jolt/ServerRouter.java
@@ -1,5 +1,7 @@
 package com.github.raffaeleragni.jolt;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -8,6 +10,9 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class ServerRouter {
+
+  private static final int MAX_BYTES = 64 * 1024;
+  private static final int BUFFER_SIZE = 8192;
 
   private final Map<String, Consumer<Envelope>> envelopeConsumers;
   private final Map<String, BiConsumer<Envelope, InputStream>> fullConsumers;
@@ -20,8 +25,28 @@ public class ServerRouter {
   }
 
   public void route(InputStream inputStream) {
-    var envelope = transformer.envelope(inputStream);
-    route(envelope.route, envelope, inputStream);
+    var bytes = inputToArray(inputStream);
+    CheckedExceptionWrapper.exwrap(() -> {
+      try (var input = new ByteArrayInputStream(bytes)) {
+        var envelope = transformer.envelope(input);
+        try (var input2 = new ByteArrayInputStream(bytes)) {
+          route(envelope.route, envelope, input2);
+        }
+      }
+    });
+  }
+
+  private byte[] inputToArray(InputStream inputStream) {
+    return CheckedExceptionWrapper.exwrap(() -> {
+      try (var array = new ByteArrayOutputStream(MAX_BYTES)) {
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int n;
+        while (-1 != (n = inputStream.read(buffer))) {
+          array.write(buffer, 0, n);
+        }
+        return array.toByteArray();
+      }
+    });
   }
 
   private <T> void route(String route, Envelope envelope, InputStream message) {
